f3ef996b5126e34e5612e27156b4b9f77d14c87e
kocakcan <can_kocak1@epam.com>
2026-07-16 16:38:29 +0300

    added depends example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 app = FastAPI(title="FastAPI Tutorial")
@@ -23,3 +23,10 @@ def get_item(item_id: int):
     if item_id not in items_db:
         return {"error": "Item not found"}
     return items_db[item_id]
+
+def get_query_param(q: str = None):
+    return q
+
+@app.get("/items/")
+def read_items(q: str = Depends(get_query_param)):
+    return {'q': q}
--- typevar.py
+++ typevar.py
@@ -1,21 +1,38 @@
-from typing import TypeVar, Generic
+# from typing import TypeVar, Generic
+#
+# T = TypeVar('T')
+#
+# class Stack(Generic[T]):
+#     def __init__(self) -> None:
+#         self._items: list[T] = []
+#
+#     def push(self, item: T) -> None:
+#         self._items.append(item)
+#
+#     def pop(self) -> T:
+#         self._items.pop()
+#
+# def first(items: list[T]) -> T:
+#     return items[0]
 
-T = TypeVar('T')
-
-class Stack(Generic[T]):
-    def __init__(self) -> None:
-        self._items: list[T] = []
-
+class Stack[T]:
     def push(self, item: T) -> None:
         self._items.append(item)
 
     def pop(self) -> T:
-        self._items.pop()
+        return self._items.pop()
+
+def first[T](items: list[T]) -> T:
+    return items[0]
 
 def main():
     s: Stack[int] = Stack()
+    items: list[str] = ["rug", "mug", "Pug"]
+    items_2: list[int] = list(range(0, 10))
     s.push(1)
     s.push('x')
+    print(f"First: {first(items)}")
+    print(f"First: {first(items_2)}")
 
 if __name__ == "__main__":
     main()
